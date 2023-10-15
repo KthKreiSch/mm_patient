61273425bf119415846588514810d236527640cc
KthKreiSch <47717521+KthKreiSch@users.noreply.github.com>
2023-10-15 14:01:03 +0200

    Renamed Questionnaire to Timeslot

---
+++ src/components/Timeslot.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import ApiService from '../services/api.service';
+let timeslots: BaseObject[] | null = await ApiService.getTimeSlots().then(res => res) ;
+
+</script>
+
+<template>
+  <v-card>
+    <v-list >
+        <v-list-item v-for="timeslot in timeslots" :key="timeslot.id" :value="timeslot.id" @click="$emit('onTimeslotChange', timeslot)">
+            <v-list-item-title>{{ timeslot.title }}</v-list-item-title>
+        </v-list-item>
+    </v-list>
+  </v-card>
+</template>
