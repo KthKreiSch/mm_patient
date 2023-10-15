674c7d671efd55e47b85e7770db216cfca3abec7
KthKreiSch <47717521+KthKreiSch@users.noreply.github.com>
2023-10-15 14:32:05 +0200

    Added TypeslotType component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,28 @@
 <script setup lang="ts">
 import { ref, type Ref } from 'vue';
 import Timeslot from './components/Timeslot.vue'
+import TimeslotType from './components/TimeslotType.vue'
 
 let selectedTimeslot: Ref<string> = ref('');
+let selectedTimeslotType: Ref<string> = ref('');
 
 </script>
 
 <template>
   <v-stepper :items="['Select time slot', 'Select time slot type', 'Select questionnaire', 'Check consent', 'Questionnaire']">
     <template v-slot:[`item.1`]>
-      <v-card title="Time slot" flat>
+      <v-card title="Time slots" flat>
         <Suspense>
-          <Timeslot @on-timeslot-change="(t) => selectedTimeslot = t"></Timeslot>
+          <Timeslot @on-timeslot-change="(t:string) => selectedTimeslot = t"></Timeslot>
         </Suspense>
       </v-card>
     </template>
 
     <template v-slot:[`item.2`]>
-      <v-card title="Time slot type" flat>
-
+      <v-card title="Time slot types" flat>
+        <Suspense>
+          <TimeslotType @on-timeslot-type-change="(t: string) => selectedTimeslotType = t"></TimeslotType>
+        </Suspense>
       </v-card>
     </template>
 
---
+++ src/components/TimeslotType.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import ApiService from '../services/api.service';
+let timeslotTypes: BaseObject[] | null = await ApiService.getTimeSlotTypes().then(res => res) ;
+
+</script>
+
+<template>
+  <v-card>
+    <h4>What type of time slot should be chosen?</h4>
+    <v-radio-group>
+        <v-radio  v-for="timeslotType in timeslotTypes" :label="timeslotType.title" :value="timeslotType.id" @click="$emit('onTimeslotTypeChange', timeslotType)"></v-radio>
+    </v-radio-group>
+  </v-card>
+</template>
